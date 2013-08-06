6d496c734a4935b57f5e7c4921c2ea2a480ae4ce
Zhou Honglin <zhou.honglin@gmail.com>
2013-08-06 22:53:42 +0800

    Fix Haskell parity error
    Wrong ending condition,

--- Chap5/parity.hs
+++ Chap5/parity.hs
@@ -1,8 +1,9 @@
 import Data.Bits
 
 parity1 :: Integer -> Integer
-parity1 1 = 0
-parity1 x = 0 `xor` (x .&. 1) `xor` parity1 (shiftR x 1)
+parity1 0 = 0
+parity1 x = (x .&. 1) `xor` parity1 (shiftR x 1)
 
 main = do
+     print (parity1 8)
      print (parity1 9)
