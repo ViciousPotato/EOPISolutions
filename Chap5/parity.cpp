b4234121dfa6ffcce39b9ca1732fbbcea49b192c
Zhou Honglin <zhou.honglin@gmail.com>
2013-08-07 00:06:36 +0800

    Add cpp parity2

--- Chap5/parity.cpp
+++ Chap5/parity.cpp
@@ -11,7 +11,21 @@ short parity1(unsigned long x) {
   return result;
 }
 
+short parity2(unsigned long x) {
+  short result = 0;
+
+  while (x) {
+    result ^= 1;
+    x &= (x-1);
+  }
+
+  return result;
+}
+
 int main() {
-  std::cout << parity1(9) << std::endl;
+  std::cout << "parity1 9 = " << parity1(9) << std::endl;
+  std::cout << "parity1 8 = " << parity1(8) << std::endl;
+  std::cout << "parity2 9 = " << parity2(9) << std::endl;
+  std::cout << "parity2 8 = " << parity2(8) << std::endl;
   return 0;
 }
