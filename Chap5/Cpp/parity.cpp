65f8eda1c8f175692e5857fd6f1e687505407bec
Zhou Honglin <zhou.honglin@gmail.com>
2013-08-15 00:18:52 +0800

    Add faster parity solution to Cpp

--- Chap5/Cpp/parity.cpp
+++ Chap5/Cpp/parity.cpp
@@ -22,10 +22,28 @@ short parity2(unsigned long x) {
   return result;
 }
 
+short *precomputed_parity;
+
+short parity3(unsigned long x) {
+  return precomputed_parity[x >> 48] ^
+    precomputed_parity[(x >> 32) & 0xFFFF] ^
+    precomputed_parity[(x >> 16) & 0xFFFF] ^
+    precomputed_parity[x & 0xFFFF];
+}
+
 int main() {
   std::cout << "parity1 9 = " << parity1(9) << std::endl;
   std::cout << "parity1 8 = " << parity1(8) << std::endl;
   std::cout << "parity2 9 = " << parity2(9) << std::endl;
   std::cout << "parity2 8 = " << parity2(8) << std::endl;
+
+  // Calculate precomputed parity array.
+  precomputed_parity = (short *)malloc(0xFFFF+1);
+  for (int i = 0; i <= 0xFFFF; i++) {
+    precomputed_parity[i] = parity1(i);
+  }
+  std::cout << "parity3 9 = " << parity3(9) << std::endl;
+  std::cout << "parity3 8 = " << parity3(8) << std::endl;
+  free(precomputed_parity);
   return 0;
 }
